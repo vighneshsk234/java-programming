f9e40403edffb1eb0b409c61bf4742f713b71bd7
vighneshsk234 <vighneshskamate30@gmail.comm>
2026-04-12 12:22:17 +0530

    addedd variable folder

---
+++ java-topicwise/02-variable/variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10; // integer variable
+        double b = 3.14; // double variable
+        char c = 'A'; // character variable
+        String d = "Hello"; // string variable
+
+        System.out.println("Integer variable a: " + a);
+        System.out.println("Double variable b: " + b);
+        System.out.println("Character variable c: " + c);
+        System.out.println("String variable d: " + d);
+    }    
+}
