8359257cd28ea2db78848d391e503a4228aec509
vighneshsk234 <vighneshskamate30@gmail.comm>
2026-04-12 12:02:59 +0530

    Added intro folder

---
+++ java-topicwise/01-Intro/intro.java
@@ -0,0 +1,5 @@
+public class intro {
+    public  static void main(String[] args) {
+        System.out.println("Hello there! This is first Java program.");
+    }    
+}
---
+++ test/test.java
@@ -0,0 +1,5 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello there! This is a test sample.");
+    }
+}
